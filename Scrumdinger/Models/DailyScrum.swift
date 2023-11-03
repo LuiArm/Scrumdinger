7fbad2025bc287615854a0a1859088a797f64b0b
LuiArm <Luisarmendariz24@icloud.com>
2023-11-02 21:51:45 -0600

    Add colors for extension, cardview, theme model

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,38 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by luis armendariz on 11/2/23.
+//
+
+import Foundation
+
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+// Sample data for daily scrum
+
+extension DailyScrum {
+    //array of daily scrums
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+    
+}
