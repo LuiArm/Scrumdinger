ffeb5b8b437253d6ac4ba84bbe6cccd66524f03e
LuiArm <Luisarmendariz24@icloud.com>
2023-11-10 22:44:21 -0700

    passing bindings to list view to choose theme

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.sampleData
+    
     var body: some Scene {
         //views added to WindowGroup scene builder are presented in a window that fills the devices entire screen
         WindowGroup {
             //Making ScrumsView the root view
-            ScrumsView(scrums: DailyScrum.sampleData)
+            ScrumsView(scrums: $scrums)
         }
     }
 }
