373f83dbe9e0523bc6f28a49e4e2880cb2ed9b78
LuiArm <Luisarmendariz24@icloud.com>
2023-11-12 15:52:23 -0700

    meeting view header, Scrum timer  class

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    let theme: Theme
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
     var body: some View {
-        
         VStack{
-            ProgressView(value: 5, total: 15)
+            ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             
             HStack{
                 VStack(alignment: .leading){
                     Text("Seconds Elapsed")
                         .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
                 }
                 
                 Spacer()
@@ -25,16 +42,19 @@ struct MeetingHeaderView: View {
                 VStack(alignment: .trailing){
                     Text("Seconds Remaining")
                         .font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time Remaining")
-        .accessibilityValue("10 Minutes")  
+        .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top,.horizontal])
     }
 }
 
 #Preview {
-    MeetingHeaderView()
+    MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
+        .previewLayout(.sizeThatFits)
 }
