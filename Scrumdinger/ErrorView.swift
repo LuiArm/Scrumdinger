7dd4351dc4cfa175e8643adb70b1c9eb19e56591
LuiArm <Luisarmendariz24@icloud.com>
2023-11-22 21:05:21 -0700

    Handling errors

--- Scrumdinger/ErrorView.swift
+++ Scrumdinger/ErrorView.swift
@@ -10,34 +10,44 @@ import SwiftUI
 
 struct ErrorView: View {
     let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack{
-            Text("An error has occurred!")
-                .font(.title)
-                .padding(.bottom)
-            Text(errorWrapper.error.localizedDescription)
-                .font(.headline)
-            Text(errorWrapper.guidance)
-                .font(.caption)
-                .padding(.top)
-            Spacer()
+        NavigationStack{
+            
+            VStack{
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .clipShape(.rect(cornerRadius: 16))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing){
+                    Button("Dismiss"){
+                        dismiss()
+                    }
+                }
+            }
         }
-        .padding()
-        .background(.ultraThinMaterial)
-        .clipShape(.rect(cornerRadius: 16))
-        
     }
 }
 
-#Preview {
-    enum SampleError: Error {
-        case errorRequired
-    }
-    
-    var wrapper: ErrorWrapper {
-        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
-    }
-    
-    ErrorView(errorWrapper: wrapper)
-}
+//#Preview {
+//    enum SampleError: Error {
+//        case errorRequired
+//    }
+//    
+//    var wrapper: ErrorWrapper {
+//        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+//    }
+//    
+//    ErrorView(errorWrapper: wrapper)
+//}
